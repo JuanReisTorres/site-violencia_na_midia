201e432c555098fd1cb9443bb78b7e2fef702812
Juan <contatojuanzito@gmail.com>
2020-10-23 10:34:23 -0300

    Novas funcionabilidades/correção de bugs/preparação para proxima atualização

--- depoimentos/mostrarDepoimentos.php
+++ depoimentos/mostrarDepoimentos.php
@@ -5,25 +5,19 @@
         $pagina_atual = $_GET['pagina']; //Vai transformar a pagina atual, em pagina atual
         $min = ($pagina_atual * $maxpage) - $maxpage;
         /* Calculará o registro (linha) mínimo para ser exibido, será pagina atual vezes quantidade de itens a serem exibidos menos quantidade de itens a serem exibidos, simplificando, se a pagina for = 3;
-            (3*10) - 10 = 20
-            Ou seja, o registro minimo que deve ser exibido é a partir do registro 20
-        */
-        $limit = $pagina_atual * $maxpage;
-        /* Calculará o registro(linha) máximo que pode ser exibido na página. Exemplo, se a pagina for = 3;
-            3 * 10 = 30
-            Ou seja, até o registro 30 deverá ser exibido
-            Como anteriormente foi calculado que o mínimo é 20 e agora foi calculado que o máximo é 30, temos uma
-            interpolação de 10 registros, que seria o máximo por página :)
+            (3*5) - 5 = 10
+            Ou seja, o registro minimo que deve ser exibido é a partir do registro 10
         */
     }else{ //Caso estejamos na primeira página
         $pagina_atual = 1; //Começaremos pela página 1
         $min = 0; //Minimo 0
-        $limit = $pagina_atual * $maxpage; // Até o 10
     }
-    $sql = "select * from depoimentos where depstatus='1' LIMIT ".$min.",".$limit.""; //Seleciona os campos com LIMIT já predefindos
+
+    $sql_linhas_totais = "select depid from depoimentos;";
+    $resultado_linhas = $conexao->query($sql_linhas_totais);
+    $sql = "select * from depoimentos where depstatus='1' LIMIT ".$maxpage." OFFSET ".$min.";"; /*Seleciona os campos com LIMIT já predefindos e o OFFSET diz para saltar esta quantidade de linhas antes de começar a retornar as linhas. */
     $resultado = $conexao->query($sql);
     if($resultado->num_rows > 0){ //Se a quantidade de linhas forem maior que 0
-        echo "Exibindo de " .$min." até ".$limit."<br><br>";
         while($linha = $resultado->fetch_assoc()){ //Recebendo a array resultado
             echo 'Depoimento ID:' . $linha["depid"].'<br>';
             echo 'Relato: ' . mb_strimwidth($linha["relato"], 0, 10, "...").'<br>';
@@ -31,15 +25,28 @@
             echo "<br>";
         }
     }else{
-        echo "Não encontramos depoimentos para serem mostrados";
+        echo "Não encontramos depoimentos para serem mostrados <br>";
     }
     $conexao -> close();
-    if($resultado->num_rows > $min){ //Caso a quantidade de linhas for maior que o minimo cria um link p/ proxima pagina
-        echo '<a href="?pagina='.($pagina_atual+1).'">Proxima página</a> <br>'; 
-        if($_GET['pagina'] > 1){
-            echo '<a href="?pagina='.($pagina_atual-1).'">Página anterior</a>';
-        }
-    }else{
-        echo '<a href="?pagina='.($pagina_atual-1).'">Página anterior</a>';
+    $qntde_pages = ceil($resultado_linhas->num_rows/$maxpage); //Calcular número de páginas, arrendondandos (resolver possíveis bug)
+
+    //Mostrar quantidade de páginas
+    for($i=1;$i <= $qntde_pages; $i++){
+        echo '<a href="?pagina='.$i.'">Página'.$i.'</a> <br>';
+        
     }
+    //Primeira pagina e ultima pagina sem Próxima/Anterior
+    switch($pagina_atual){
+        case 1:
+            echo '<br><a href="?pagina='.($pagina_atual+1).'">Próxima página</a> <br>';
+            break;
+        case $qntde_pages:
+            echo '<br><a href="?pagina='.($pagina_atual-1).'">Anterior</a><br>';
+            break;
+        default:
+            echo '<br><a href="?pagina='.($pagina_atual+1).'">Próxima página</a><br>';
+            echo '<a href="?pagina='.($pagina_atual-1).'">Anterior</a><br>';
+            break;
+    }
+
 ?>
